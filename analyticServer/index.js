12280d784fc7afa193a1cafec144f970912616ed
dorofii <dorofiy@seriouscake.net>
2017-05-25 13:06:35 +0300

    fix

--- analyticServer/index.js
+++ analyticServer/index.js
@@ -31,115 +31,120 @@ const server = http.createServer((req, res) => {
         chunks.push(chunk)
     });
     req.on("end", () => {
-        const buffer = Buffer.concat(chunks);
-        const jsonString = String(buffer);
+        try {
+            const buffer = Buffer.concat(chunks);
+            const jsonString = String(buffer);
 
-        const json = JSON.parse(jsonString);
+            const json = JSON.parse(jsonString);
 
-        const eventName = json.eventName;
-        const userId = json.userId;
-        const platform = json.platform;
-        const deviceModel = json.deviceModel;
-        const deviceName = json.deviceName;
-        const deviceType = json.deviceType;
-        const deviceUniqueIdentifier = json.deviceUniqueIdentifier;
-        const operatingSystem = json.operatingSystem;
-        const systemMemorySize = json.systemMemorySize;
-        const graphicsDeviceID = json.graphicsDeviceID;
-        const graphicsDeviceName = json.graphicsDeviceName;
-        const graphicsDeviceType = json.graphicsDeviceType;
-        const graphicsDeviceVendor = json.graphicsDeviceVendor;
-        const graphicsShaderLevel = json.graphicsShaderLevel;
-        const screenDpi = json.screenDpi;
-        const screenWidth = json.screenWidth;
-        const screenHeight = json.screenHeight;
-        const loginToken = json.loginToken;
-        const playerName = json.playerName;
-        const deviceTime = json.deviceTime;
-        const timeSourceTicks = json.timeSourceTicks;
-        const advertisingId = json.advertisingId;
-        const serverHostName = json.serverHostName;
-        const remoteAddress = req.socket.remoteAddress;
-        const remotePort = Number(req.socket.remotePort);
-        const remoteFamily = req.socket.remoteFamily;
-        const data = JSON.stringify(json.data);
-        const header = JSON.stringify(req.headers);
+            const eventName = json.eventName;
+            const userId = json.userId;
+            const platform = json.platform;
+            const deviceModel = json.deviceModel;
+            const deviceName = json.deviceName;
+            const deviceType = json.deviceType;
+            const deviceUniqueIdentifier = json.deviceUniqueIdentifier;
+            const operatingSystem = json.operatingSystem;
+            const systemMemorySize = json.systemMemorySize;
+            const graphicsDeviceID = json.graphicsDeviceID;
+            const graphicsDeviceName = json.graphicsDeviceName;
+            const graphicsDeviceType = json.graphicsDeviceType;
+            const graphicsDeviceVendor = json.graphicsDeviceVendor;
+            const graphicsShaderLevel = json.graphicsShaderLevel;
+            const screenDpi = json.screenDpi;
+            const screenWidth = json.screenWidth;
+            const screenHeight = json.screenHeight;
+            const loginToken = json.loginToken;
+            const playerName = json.playerName;
+            const deviceTime = json.deviceTime;
+            const timeSourceTicks = json.timeSourceTicks;
+            const advertisingId = json.advertisingId;
+            const serverHostName = json.serverHostName;
+            const remoteAddress = req.socket.remoteAddress;
+            const remotePort = Number(req.socket.remotePort);
+            const remoteFamily = req.socket.remoteFamily;
+            const data = JSON.stringify(json.data);
+            const header = JSON.stringify(req.headers);
 
-        var keyValue = [
-            { "UserId": userId },
-            { "EventName": eventName },
-            { "AdvertisingId": advertisingId },
-            { "TimeSourceTicks": timeSourceTicks },
-            { "DeviceTime": deviceTime },
-            { "ServerHostname": serverHostName },
-            { "PlayerName": playerName },
-            { "LoginToken": loginToken },
-            { "Platform": platform },
-            { "DeviceModel": deviceModel },
-            { "DeviceName": deviceName },
-            { "DeviceType": deviceType },
-            { "DeviceUniqueIdentifier": deviceUniqueIdentifier },
-            { "OperatingSystem": operatingSystem },
-            { "SystemMemorySize": systemMemorySize },
-            { "GraphicsDeviceID": graphicsDeviceID },
-            { "GraphicsDeviceName": graphicsDeviceName },
-            { "GraphicsDeviceType": graphicsDeviceType },
-            { "GraphicsDeviceVendor": graphicsDeviceVendor },
-            { "GraphicsShaderLevel": graphicsShaderLevel },
-            { "ScreenDpi": screenDpi | 0 },
-            { "ScreenWidth": screenWidth },
-            { "ScreenHeight": screenHeight },
-            { "Data": data },
-            { "Headers": header },
-            { "Address": remoteAddress },
-            { "Port": remotePort },
-            { "Family": remoteFamily }
-        ];
+            var keyValue = [
+                { "UserId": userId },
+                { "EventName": eventName },
+                { "AdvertisingId": advertisingId },
+                { "TimeSourceTicks": timeSourceTicks },
+                { "DeviceTime": deviceTime },
+                { "ServerHostname": serverHostName },
+                { "PlayerName": playerName },
+                { "LoginToken": loginToken },
+                { "Platform": platform },
+                { "DeviceModel": deviceModel },
+                { "DeviceName": deviceName },
+                { "DeviceType": deviceType },
+                { "DeviceUniqueIdentifier": deviceUniqueIdentifier },
+                { "OperatingSystem": operatingSystem },
+                { "SystemMemorySize": systemMemorySize },
+                { "GraphicsDeviceID": graphicsDeviceID },
+                { "GraphicsDeviceName": graphicsDeviceName },
+                { "GraphicsDeviceType": graphicsDeviceType },
+                { "GraphicsDeviceVendor": graphicsDeviceVendor },
+                { "GraphicsShaderLevel": graphicsShaderLevel },
+                { "ScreenDpi": screenDpi | 0 },
+                { "ScreenWidth": screenWidth },
+                { "ScreenHeight": screenHeight },
+                { "Data": data },
+                { "Headers": header },
+                { "Address": remoteAddress },
+                { "Port": remotePort },
+                { "Family": remoteFamily }
+            ];
 
-        var keys = [];
-        var values = [];
+            var keys = [];
+            var values = [];
 
-        for (var i = 0; i < keyValue.length; i++) {
-            var element = keyValue[i];
-            for (var key in element) {
-                var value = element[key];
-                keys[i] = key;
-                values[i] = value;
-            }
-        }
-
-        for (var i = 0; i < values.length; i++) {
-            var element = values[i];
-            values[i] = formatData(element);
-        }
-
-        console.assert(keys.length == values.length, "keys.length != values.length");
-
-        var query = util.format("INSERT INTO dbo.Event (%s) VALUES (%s)", keys.join(", "), values.join(", "));
-
-        function sqlRequest() {
-            new sql.Request().query(query, (err, result) => {
-                if (err) {
-                    console.error("sqlRequest-error:" + err.message);
-                    if (err.message.indexOf("Connection is closed") != -1 || err.message.indexOf("No connection") != -1) {
-                        sqlConnect(sqlRequest);
-                    }
-                } else {
-                    console.log("sqlRequest-success: eventName:" + eventName);
+            for (var i = 0; i < keyValue.length; i++) {
+                var element = keyValue[i];
+                for (var key in element) {
+                    var value = element[key];
+                    keys[i] = key;
+                    values[i] = value;
                 }
-            });
+            }
+
+            for (var i = 0; i < values.length; i++) {
+                var element = values[i];
+                values[i] = formatData(element);
+            }
+
+            console.assert(keys.length == values.length, "keys.length != values.length");
+
+            var query = util.format("INSERT INTO dbo.Event (%s) VALUES (%s)", keys.join(", "), values.join(", "));
+
+            function sqlRequest() {
+                new sql.Request().query(query, (err, result) => {
+                    if (err) {
+                        console.error("sqlRequest-error:" + err.message);
+                        if (err.message.indexOf("Connection is closed") != -1 || err.message.indexOf("No connection") != -1) {
+                            sqlConnect(sqlRequest);
+                        }
+                    } else {
+                        console.log("sqlRequest-success: eventName:" + eventName);
+                    }
+                });
+            }
+
+            sqlRequest();
+
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('ok');
+        } catch (exc) {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('fail');
         }
-
-        sqlRequest();
-
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('ok');
     });
 });
 
 function sqlConnect(onConnect) {
     if (!dbConnectId) {
-        console.log("try to connect to the database");
+        console.log("try to connect to the database:" + config);
         sql.connect(config, error => {
             if (!server.listening) {
                 server.listen(port, host, () => {
